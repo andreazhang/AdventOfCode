2662e8b0dded54689da5bf5b836c88ffe9e27d36
Andrea <zhang.andrea@outlook.com>
2024-11-28 19:09:49 +0000

    add more scenario for find number in text

--- src/test/kotlin/NumberInStringTest.kt
+++ src/test/kotlin/NumberInStringTest.kt
@@ -1,4 +1,4 @@
-import org.example.NumberInString
+import org.example.NumberInString.Companion.findFirstNumberInText
 import org.junit.jupiter.api.Test;
 import kotlin.test.assertEquals
 
@@ -8,8 +8,26 @@ class NumberInStringTest {
     fun `find first number in text`() {
         val text = "1asdf"
 
-        val number = NumberInString.findFirstNumberInText(text)
+        val number = findFirstNumberInText(text)
 
         assertEquals(number, 1)
+    }
+
+    @Test
+    fun `find first number in middle of text`() {
+        val text = "asd2f"
+
+        val number = findFirstNumberInText(text)
+
+        assertEquals(number, 2)
+    }
+
+    @Test
+    fun `return 0 if no number found in text`() {
+        val text = "asdf"
+
+        val number = findFirstNumberInText(text)
+
+        assertEquals(number, 0)
     }
 }
