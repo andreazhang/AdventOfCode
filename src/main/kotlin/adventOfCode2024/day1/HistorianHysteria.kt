d358b1acd327360458a7b1df988f264bbe662c37
Andrea <zhang.andrea@outlook.com>
2024-12-01 09:23:05 +0000

    basic test

---
+++ src/main/kotlin/adventOfCode2024/day1/HistorianHysteria.kt
@@ -0,0 +1,14 @@
+package adventOfCode2024.day1
+
+class HistorianHysteria {
+    companion object {
+        fun parseInput(input: String): Locations {
+            TODO("Not yet implemented")
+        }
+    }
+
+    class Locations(val left: List<Int>, val right: List<Int>) {
+
+    }
+
+}
--- src/test/kotlin/adventOfCode2024/day1/HistorianHysteriaTest.kt
+++ src/test/kotlin/adventOfCode2024/day1/HistorianHysteriaTest.kt
@@ -1,11 +1,17 @@
 package adventOfCode2024.day1
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class HistorianHysteriaTest {
 
     @Test
-    fun `test`() {
+    fun `parse basic input`() {
+        val input = this::class.java.getResource("/adventOfCode2024/day1/basic.txt")?.readText() ?: fail()
 
+        val locations = HistorianHysteria.parseInput(input)
+
+        assertEquals(listOf(3, 4,2,1,3,3), locations.left)
     }
 }
