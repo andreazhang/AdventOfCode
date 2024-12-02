4f1f85f8db75cb8752cdbcb33293f99722a532ec
Andrea <zhang.andrea@outlook.com>
2024-12-02 18:40:24 +0000

    parse input

--- src/main/kotlin/adventOfCode2024/day1/HistorianHysteria.kt
+++ src/main/kotlin/adventOfCode2024/day1/HistorianHysteria.kt
@@ -3,7 +3,12 @@ package adventOfCode2024.day1
 class HistorianHysteria {
     companion object {
         fun parseInput(input: String): Locations {
-            TODO("Not yet implemented")
+            val lines = input.split("\r\n").filter { it.isNotEmpty() }
+            val linesSplitToNumberArray = lines.map { line -> line.split(" ").filter { it.isNotEmpty() } }
+            val leftLocations = linesSplitToNumberArray.map { it[0].toInt() }.toList()
+            val rightLocations = linesSplitToNumberArray.map { it[1].toInt() }.toList()
+
+            return Locations(leftLocations, rightLocations)
         }
     }
 
