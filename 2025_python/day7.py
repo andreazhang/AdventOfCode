a08960d9525fa48c8cc3f1880159f1b8dddb14f2
andreazhang <zhang.andrea@outlook.com>
2025-12-07 15:31:36 +0000

    cleanup

--- 2025_python/day7.py
+++ 2025_python/day7.py
@@ -32,26 +32,23 @@ print("Start point at x =", startPoint)
 for line in grid:
     print(''.join(line))
 
-numberOfTachyonBeams = 0
-
-def moveTachyonBeamDown(grid, x, y):
+def moveTachyonBeamDownAndCountSplits(grid, x, y):
     # print("Moving tachyon beam to", x, y)
-    counter = 0
     if y >= height:
-        return counter
+        return 0
     if grid[y][x] == '.':
         grid[y][x] = '|'
-        return counter + moveTachyonBeamDown(grid, x, y + 1)
+        return moveTachyonBeamDownAndCountSplits(grid, x, y + 1)
     elif grid[y][x] == '^':
             grid[y][x-1] = '|'
             grid[y][x+1] = '|'
-            return counter + 1 + moveTachyonBeamDown(grid, x - 1, y+1) + moveTachyonBeamDown(grid, x + 1, y+1)
+            return 1 + moveTachyonBeamDownAndCountSplits(grid, x - 1, y + 1) + moveTachyonBeamDownAndCountSplits(grid, x + 1, y + 1)
     return 0
 
-numberOfTachyonBeams = moveTachyonBeamDown(grid, startPoint, 1)
+numberOfTachyonBeamsSplits = moveTachyonBeamDownAndCountSplits(grid, startPoint, 1)
 
 print("Grid after moving tachyon beam:")
 for line in grid:
     print(''.join(line))
 
-print("Part 1: Number of tachyon beams encountered =", numberOfTachyonBeams)
+print("Part 1: Number of tachyon beams splits =", numberOfTachyonBeamsSplits)
